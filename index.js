9bc29d067d6d5b83719a97e2dd5f33c444730e4f
Nicolas Perriault <nicolas@perriault.net>
2025-09-28 17:08:20 +0200

    feat: post mastodon toot

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const movies = require("./db.json");
+import movies from "./db.json" with { type: "json" };
+import fs from "node:fs/promises";
+import path from "path";
+import { createRestAPIClient } from "masto";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.URL) {
+  throw new Error("URL is missing");
+}
+if (!process.env.TOKEN) {
+  throw new Error("TOKEN is missing");
+}
+
+const client = createRestAPIClient({
+  url: process.env.URL,
+  accessToken: process.env.TOKEN,
+});
 
 function dayOfYear(date) {
   return Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
@@ -14,12 +32,45 @@ function formatDuration(duration) {
 
 function format(movie) {
   return [
-    `${movie.alternateName} (${movie.name} | ${movie.year} | ${formatDuration(movie.duration)})`,
-    `${movie.description}`,
-    `${movie.rating}/10 - ${movie.genre} - ${movie.contentRating}`,
+    `${movie.alternateName} (${movie.name} | ${movie.year} | ${formatDuration(movie.duration)})\n`,
+    `${movie.description}\n`,
+    `${movie.rating}/10 - ${movie.genre} - ${movie.contentRating} #seance21h`,
     `${movie.url}`,
   ].join("\n");
 }
 
-const randomMovie = pick(new Date());
-console.log(format(randomMovie));
+async function tootMovieOfTheDay(date) {
+  const randomMovie = pick(date);
+  const tootText = format(randomMovie);
+
+  const imageFile = await fs.readFile(path.join(process.cwd(), "img", `${randomMovie.image}.jpg`));
+  const attachment = await client.v2.media.create({
+    file: new Blob([imageFile], { type: "image/jpeg" }),
+    description: `Affiche du film ${randomMovie.alternateName}`,
+  });
+
+  const { url } = await createToot({
+    status: tootText,
+    visibility: process.env.VISIBILITY || "direct",
+    mediaIds: [attachment.id],
+  });
+  console.log(`Movie of the day posted:\n${tootText}\n${url}`);
+}
+
+async function createToot(params, retries = 3, backoff = 500) {
+  const retryCodes = [408, 500, 502, 503, 504, 522, 524];
+  try {
+    return client.v1.statuses.create(params);
+  } catch (err) {
+    console.warn(err.message);
+    if (retries > 0 && retryCodes.includes(err.statusCode || 503)) {
+      setTimeout(() => {
+        return createToot(params, retries - 1, backoff * 2);
+      }, backoff);
+    } else {
+      throw err;
+    }
+  }
+}
+
+tootMovieOfTheDay(new Date());
